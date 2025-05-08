e8819fb772991aadb8b1156945c3a2423573bb2c
BelyaevDmitryi <32246526+BelyaevDmitryi@users.noreply.github.com>
2025-05-08 16:57:44 +0300

    hw03. Исправил на конструктор

--- hw03/src/main/java/ru/otus/hw/config/AppProperties.java
+++ hw03/src/main/java/ru/otus/hw/config/AppProperties.java
@@ -1,13 +1,14 @@
 package ru.otus.hw.config;
 
 import lombok.Getter;
-import lombok.Setter;
+import lombok.ToString;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.ConstructorBinding;
 
 import java.util.Locale;
 import java.util.Map;
 
-@Setter
+@ToString
 @ConfigurationProperties(prefix = "test")
 public class AppProperties implements TestConfig, TestFileNameProvider, LocaleConfig {
 
@@ -18,6 +19,17 @@ public class AppProperties implements TestConfig, TestFileNameProvider, LocaleCo
     private Locale locale;
 
     private Map<String, String> fileNameByLocaleTag;
+
+    @ConstructorBinding
+    public AppProperties(
+            int rightAnswersCountToPass,
+            String locale,
+            Map<String, String> fileNameByLocaleTag
+    ) {
+        this.rightAnswersCountToPass = rightAnswersCountToPass;
+        this.locale = Locale.forLanguageTag(locale);
+        this.fileNameByLocaleTag = fileNameByLocaleTag;
+    }
 
     public void setLocale(String locale) {
         this.locale = Locale.forLanguageTag(locale);
