23e75862b3e4b7377b355bfa23f555d6e624310e
BelyaevDmitryi <32246526+BelyaevDmitryi@users.noreply.github.com>
2025-06-03 13:51:21 +0300

    hw06. Added test

--- hw06/src/main/java/ru/otus/hw/services/BookServiceImpl.java
+++ hw06/src/main/java/ru/otus/hw/services/BookServiceImpl.java
@@ -2,6 +2,7 @@ package ru.otus.hw.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.otus.hw.exceptions.EntityNotFoundException;
 import ru.otus.hw.models.Book;
 import ru.otus.hw.repositories.AuthorRepository;
@@ -9,7 +10,6 @@ import ru.otus.hw.repositories.BookRepository;
 import ru.otus.hw.repositories.GenreRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -21,8 +21,9 @@ public class BookServiceImpl implements BookService {
     private final BookRepository bookRepository;
 
     @Override
-    public Optional<Book> findById(long id) {
-        return bookRepository.findById(id);
+    public Book findById(long id) {
+        return bookRepository.findById(id).
+                orElseThrow(() -> new EntityNotFoundException("Book with id %d not found".formatted(id)));
     }
 
     @Override
@@ -31,26 +32,32 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
+    @Transactional
     public Book insert(String title, long authorId, long genreId) {
-        return save(0, title, authorId, genreId);
+        var author = authorRepository.findById(authorId).orElseThrow(() -> new EntityNotFoundException("Author with id %d not found".formatted(authorId)));
+        var genre = genreRepository.findById(genreId).orElseThrow(() -> new EntityNotFoundException("Genre with id %d not found".formatted(genreId)));
+        var book = new Book(0, title, author, genre);
+        return bookRepository.save(book);
     }
 
     @Override
+    @Transactional
     public Book update(long id, String title, long authorId, long genreId) {
-        return save(id, title, authorId, genreId);
-    }
-
-    @Override
-    public void deleteById(long id) {
-        bookRepository.deleteById(id);
-    }
-
-    private Book save(long id, String title, long authorId, long genreId) {
+        var book = bookRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Book with id %d not found".formatted(id)));
         var author = authorRepository.findById(authorId)
                 .orElseThrow(() -> new EntityNotFoundException("Author with id %d not found".formatted(authorId)));
         var genre = genreRepository.findById(genreId)
                 .orElseThrow(() -> new EntityNotFoundException("Genre with id %d not found".formatted(genreId)));
-        var book = new Book(id, title, author, genre);
+
+        book.setAuthor(author);
+        book.setGenre(genre);
+        book.setTitle(title);
         return bookRepository.save(book);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(long id) {
+        bookRepository.deleteById(id);
     }
 }
---
+++ hw06/src/test/java/ru/otus/hw/repositories/JpaGenreRepositoryTest.java
@@ -0,0 +1,44 @@
+package ru.otus.hw.repositories;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.context.annotation.Import;
+import ru.otus.hw.models.Genre;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Репозиторий на основе JPA для работы с жанрами ")
+@DataJpaTest
+@Import({JpaGenreRepository.class})
+class JpaGenreRepositoryTest {
+
+    private static final long FIRST_ID = 1;
+
+    @Autowired
+    private JpaGenreRepository repository;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @DisplayName("должен загружать список всех жанров")
+    @Test
+    void shouldReturnCorrectGenresList() {
+        var actualGenres = repository.findAll();
+
+        assertThat(actualGenres).isNotNull()
+                .allMatch(genre -> genre.getName() != null && !genre.getName().isEmpty());
+    }
+
+    @DisplayName("должен загружать жанр по id")
+    @Test
+    void shouldReturnCorrectGenreById() {
+        var actualGenre = repository.findById(FIRST_ID);
+        var expectedGenre = entityManager.find(Genre.class, FIRST_ID);
+        assertThat(actualGenre).isPresent()
+                .get()
+                .isEqualTo(expectedGenre);
+    }
+}
