abf5b9575b1014dab50b052240c5691f5692fd68
BelyaevDmitryi <32246526+BelyaevDmitryi@users.noreply.github.com>
2025-08-26 11:44:57 +0300

    hw13. Without Acl and tests

---
+++ hw13/src/main/java/ru/otus/hw/security/SecurityConfiguration.java
@@ -0,0 +1,36 @@
+package ru.otus.hw.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+                .authorizeHttpRequests((authorize) -> authorize
+//                        .requestMatchers("/comment/book/*").authenticated()
+                        .requestMatchers("/book/**").hasAuthority("ROLE_BOOK_EDITOR")
+//                        .requestMatchers("/comment/**").hasAuthority("ROLE_COMMENT_EDITOR")
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(13);
+    }
+}
