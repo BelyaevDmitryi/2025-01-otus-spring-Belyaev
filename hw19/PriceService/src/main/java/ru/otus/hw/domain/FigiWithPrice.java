8f7c6937812dd4d1d298f2b7a37f2bdd2ea60b41
Dmitry Belyaev <dmitry.belyaev@lab-a.pro>
2025-10-04 11:49:28 +0300

    hw19. Fix hw19

--- hw19/PriceService/src/main/java/ru/otus/hw/domain/FigiWithPrice.java
+++ hw19/PriceService/src/main/java/ru/otus/hw/domain/FigiWithPrice.java
@@ -2,6 +2,7 @@ package ru.otus.hw.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NonNull;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
@@ -15,5 +16,6 @@ public class FigiWithPrice {
     @Id
     private String figi;
     private BigDecimal price;
+    @NonNull
     private String source;
 }
